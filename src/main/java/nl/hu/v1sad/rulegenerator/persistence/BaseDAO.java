26d460a3c037d61426fcdc05ed1a1fb274dc9556
Kevin984 <kevinbalk14@gmail.com>
2018-01-28 12:09:31 +0100

    ok

--- src/main/java/nl/hu/v1sad/rulegenerator/persistence/BaseDAO.java
+++ src/main/java/nl/hu/v1sad/rulegenerator/persistence/BaseDAO.java
@@ -1,15 +1,17 @@
 package nl.hu.v1sad.rulegenerator.persistence;
 
 import java.net.URI; import java.sql.Connection;
-import java.util.Hashtable;
-
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import javax.naming.Context;
-import javax.naming.InitialContext; import javax.sql.DataSource;
+import javax.naming.InitialContext; 
+import javax.sql.DataSource;
 import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 public class BaseDAO {
  private static DataSource connectionPool;
 
  public BaseDAO() {
+	/* 
  if (connectionPool == null) {
  try {
  final String DATABASE_URL_PROP = System.getenv("DATABASE_URL");
@@ -32,17 +34,30 @@ public class BaseDAO {
  pool.setInitialSize(1);
 
  connectionPool = pool;
+ System.out.print(DATABASE_URL_PROP);
  } else { // de applicatie draait lokaal
- //InitialContext ic = new InitialContext();
- //connectionPool = (DataSource)ic.lookup("java:comp/env/jdbc/OracleDS");
+	 System.out.print(DATABASE_URL_PROP + " 22222");
+
+ InitialContext ic = new InitialContext();
+ connectionPool = (DataSource)ic.lookup("java:comp/env/jdbc/OracleDS");
  }
  } catch (Exception e) { throw new RuntimeException(e); }
- }
+ }*/
  }
  
  protected final Connection getRepositoryConnection() {
- try {
- return connectionPool.getConnection();
- } catch (Exception ex) { throw new RuntimeException(ex); }
+ //try {
+//	 System.out.println(System.getenv("DATABASE_URL"));
+ //return connectionPool.getConnection();
+ Connection conn = null;
+ 
+ try{
+	 conn = DriverManager.getConnection("jdbc:oracle:thin:@ondora02.hu.nl:8521/cursus02.hu.nl", "tosad_2017_2a_team2", "tosad_2017_2a_team2");
+ }
+ catch(SQLException e){
+	 throw new RuntimeException(e);
+ }
+ return conn;
+// } catch (Exception ex) { throw new RuntimeException(ex); }
  }
 }
