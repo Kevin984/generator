99e4f552a16fcfe2f6fab3f331f2c10dd4d20567
DanielVeldkamp <daniel.veldkamp@student.hu.nl>
2018-01-29 17:42:01 +0100

    .

--- src/main/java/nl/hu/v1sad/rulegenerator/webservices/BusinessRuleResource.java
+++ src/main/java/nl/hu/v1sad/rulegenerator/webservices/BusinessRuleResource.java
@@ -1,8 +1,5 @@
 package nl.hu.v1sad.rulegenerator.webservices;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonArrayBuilder;
@@ -10,28 +7,25 @@ import javax.json.JsonObjectBuilder;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-
 import nl.hu.v1sad.rulegenerator.domain.BusinessRule;
-import nl.hu.v1sad.rulegenerator.persistence.RepositoryDatabaseDAO;
 
 @Path("/businessrules")
 public class BusinessRuleResource {
-private RepositoryDatabaseDAO repoDAO = new RepositoryDatabaseDAO();
-BusinessRuleService brService = BusinessRuleProvider.getBusinessRuleService();
+	BusinessRuleService brService = BusinessRuleProvider.getBusinessRuleService();
 
-@GET
-@Produces
-public String getBusinessRules() {
-	BusinessRuleService service = BusinessRuleProvider.getBusinessRuleService();
-	JsonArrayBuilder jab = Json.createArrayBuilder();
-	JsonObjectBuilder job = Json.createObjectBuilder();//stond eerst in for loop?	
-	for(BusinessRule br : service.getAllBusinessRules("myDatabase")) {
-		job.add("databasetype", br.getDatabaseType());
-		job.add("name", br.getName());
-		job.add("errormessage", br.getErrorMessage());
-		jab.add(job);
+	@GET
+	@Produces
+	public String getBusinessRules() {
+		BusinessRuleService service = BusinessRuleProvider.getBusinessRuleService();
+		JsonArrayBuilder jab = Json.createArrayBuilder();
+		for(BusinessRule br : service.getAllBusinessRules("myDatabase")) {
+			JsonObjectBuilder job = Json.createObjectBuilder();	
+			job.add("databasetype", br.getDatabaseType());
+			job.add("name", br.getName());
+			job.add("errormessage", br.getErrorMessage());
+			jab.add(job);
+		}
+		JsonArray array = jab.build();
+		return array.toString();
 	}
-	JsonArray array = jab.build();
-	return array.toString();
-}
 }
