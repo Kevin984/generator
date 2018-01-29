1d5c54b95212f01cafa6def0ce4400c39413dd5b
DanielVeldkamp <daniel.veldkamp@student.hu.nl>
2018-01-29 14:09:35 +0100

    added/changed dao classes

--- src/main/java/nl/hu/v1sad/rulegenerator/persistence/BaseDAO.java
+++ src/main/java/nl/hu/v1sad/rulegenerator/persistence/BaseDAO.java
@@ -1,6 +1,7 @@
 package nl.hu.v1sad.rulegenerator.persistence;
 
-import java.net.URI; import java.sql.Connection;
+import java.net.URI; 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -9,30 +10,28 @@ import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 public class BaseDAO {
  private static DataSource connectionPool;
 
- public BaseDAO() {
- }
+ 	public BaseDAO() {
+ 	}
  
- protected final Connection getRepositoryConnection() {
- Connection conn = null;
- try{
-	 conn = DriverManager.getConnection("jdbc:oracle:thin:@ondora02.hu.nl:8521/cursus02.hu.nl", "tosad_2017_2a_team2", "tosad_2017_2a_team2");
- }
- catch(SQLException e){
-	 throw new RuntimeException(e);
- }
- return conn;
- }
+ 	protected final Connection getRepositoryConnection() {
+ 		Connection conn = null;
+ 		try{
+ 			conn = DriverManager.getConnection("jdbc:oracle:thin:@ondora02.hu.nl:8521/cursus02.hu.nl", "tosad_2017_2a_team2", "tosad_2017_2a_team2");
+ 		}
+ 		catch(SQLException e){
+ 			throw new RuntimeException(e);
+ 		}
+ 		return conn;
+ 	}
  
- protected final Connection getTargetConnectionByServiceName(String driverPrefix, String url, int port, String servicenameOrSID, String username, String password) {
- Connection conn = null;
- try{
-	 conn = DriverManager.getConnection(driverPrefix+url+":"+port+"/" + servicenameOrSID, username, password); //if SID, use : instead of / (after port) ALSO: other databases may have it differently
+ 	protected final Connection getTargetConnection(String driverPrefix, String url, String port, String divider, String SIDorSERVICENAME, String username, String password) {
+ 		Connection conn = null;
+ 		try{
+ 			conn = DriverManager.getConnection(driverPrefix + url + ":" + port + divider + SIDorSERVICENAME, username, password); //if SID, use : instead of / (after port) ALSO: other databases may have it differently
+ 		}
+ 		catch(SQLException e){
+ 			throw new RuntimeException(e);
+ 		}
+ 	return conn;
  }
- catch(SQLException e){
-	 throw new RuntimeException(e);
- }
- return conn;
- }
- 
- 
 }
--- src/main/java/nl/hu/v1sad/rulegenerator/persistence/TargetDatabaseDAO.java
+++ src/main/java/nl/hu/v1sad/rulegenerator/persistence/TargetDatabaseDAO.java
@@ -1,8 +1,63 @@
 package nl.hu.v1sad.rulegenerator.persistence;
 
-public class TargetDatabaseDAO extends BaseDAO{
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+import nl.hu.v1sad.rulegenerator.persistence.RepositoryDatabaseDAO;
+
+public class TargetDatabaseDAO extends BaseDAO {
+	private RepositoryDatabaseDAO repoDAO = new RepositoryDatabaseDAO();
+	private ArrayList<String> dbInfo = new ArrayList<String>();
+	private ArrayList<String> tables = new ArrayList<String>();
+	private ArrayList<String> columns = new ArrayList<String>();
 	
+	public void executeTrigger(String databaseName, String trigger) {
+		dbInfo = repoDAO.selectTargetDBInfo(databaseName);
+		try (Connection con = super.getTargetConnection(dbInfo.get(0), dbInfo.get(1), dbInfo.get(2), dbInfo.get(3), dbInfo.get(4), dbInfo.get(5), dbInfo.get(6))){
+			Statement stmt = con.createStatement();
+			stmt.executeQuery(trigger);
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
 	
+	public ArrayList<String> getAllTablesOfDatabase(String databaseName) {
+		String query = "SELECT table_name FROM user_tables";
+		dbInfo = repoDAO.selectTargetDBInfo(databaseName);
+		tables.clear();
+		try (Connection con = super.getTargetConnection(dbInfo.get(0), dbInfo.get(1), dbInfo.get(2), dbInfo.get(3), dbInfo.get(4), dbInfo.get(5), dbInfo.get(6))){
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			while(rs.next()) {
+				String tablename = rs.getString("TABLE_NAME");
+				tables.add(tablename);
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return tables;
+	}
 	
+	public ArrayList<String> getAllColumnsOfTable(String databaseName, String tableName) {
+		String query = "SELECT COLUMN_NAME FROM USER_TAB_COLUMNS WHERE upper(table_name) = '" + tableName.toUpperCase() + "'";
+		dbInfo = repoDAO.selectTargetDBInfo(databaseName);
+		columns.clear();
+		try (Connection con = super.getTargetConnection(dbInfo.get(0), dbInfo.get(1), dbInfo.get(2), dbInfo.get(3), dbInfo.get(4), dbInfo.get(5), dbInfo.get(6))){
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			while(rs.next()) {
+				String columnname = rs.getString("COLUMN_NAME");
+				columns.add(columnname);
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return columns;
+	}
 }
