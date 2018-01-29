74472a668ae350d953bfeb0e6bd5ff0d77ca967d
DanielVeldkamp <daniel.veldkamp@student.hu.nl>
2018-01-29 17:35:53 +0100

    some small changes for testing and fixing

--- src/main/java/nl/hu/v1sad/rulegenerator/domain/Main.java
+++ src/main/java/nl/hu/v1sad/rulegenerator/domain/Main.java
@@ -3,17 +3,23 @@ package nl.hu.v1sad.rulegenerator.domain;
 import java.util.ArrayList;
 
 import nl.hu.v1sad.rulegenerator.persistence.TargetDatabaseDAO;
+import nl.hu.v1sad.rulegenerator.webservices.BusinessRuleResource;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// Main class for testing purposes
-		TargetDatabaseDAO dao = new TargetDatabaseDAO();
-		ArrayList<String> tables = dao.getAllTablesOfDatabase("myDatabase");
-		System.out.println("All tables: " + tables);
-		for (int i = 0;i<tables.size();i++) {
-			System.out.println("Columns in table " + tables.get(i) + ": " + dao.getAllColumnsOfTable("myDatabase", tables.get(i)));
-		}
+		
+		//TargetDatabaseDAO dao = new TargetDatabaseDAO();
+		//ArrayList<String> tables = dao.getAllTablesOfDatabase("myDatabase");
+		//System.out.println("All tables: " + tables);
+		//for (int i = 0;i<tables.size();i++) {
+		//	System.out.println("Columns in table " + tables.get(i) + ": " + dao.getAllColumnsOfTable("myDatabase", tables.get(i)));
+		//}
+		
+		BusinessRuleResource resource = new BusinessRuleResource();
+		
+		System.out.println(resource.getBusinessRules());
 	}
 
 }
